de2fed3b071eefded3ffc8742f6d3a7f5d710a39
Aman <sharmaaman4419@gmail.com>
2023-06-23 13:32:41 +0530

    adding the pagination for pizzas list controller

--- src/controllers/pizzaController.js
+++ src/controllers/pizzaController.js
@@ -2,9 +2,15 @@ const PizzaModel = require("../models/pizzaModel");
 
 const getAllPizzas = async (req, res, next) => {
   try {
-    const pizzas = await PizzaModel.find();
+    const page = parseInt(req.query.page) || 1; // Current page number, defaulting to 1
+    const pageSize = parseInt(req.query.pageSize) || 6; // Number of items per page, defaulting to 10
+    const totalDocsCount = await PizzaModel.countDocuments();
+    const totalPages = Math.ceil(totalDocsCount / pageSize);
+    const pizzas = await PizzaModel.find()
+      .skip((page - 1) * pageSize)
+      .limit(pageSize);
     if (pizzas) {
-      return res.status(200).json({ data: pizzas });
+      return res.status(200).json({ data: pizzas, totalPages });
     }
   } catch (error) {
     next(error);
